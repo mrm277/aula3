5089f75ac71055ce1022cb0bb1cf4f583401baae
mrm277 <107406775+mrm277@users.noreply.github.com>
2024-10-02 12:51:17 +0100

    ADD: Build já dá

---
+++ src/tests/Home.test.js
@@ -0,0 +1,11 @@
+const { render, screen } = require('@testing-library/react');
+require('@testing-library/jest-dom'); 
+const React = require('react');
+const Home = require('../pages/Home.js');
+
+test('renderiza o MyComponent', () => {
+  render(React.createElement(Home));
+
+  const element = screen.getByText(/This is a simple home page./i);
+  expect(element).toBeInTheDocument();
+});
