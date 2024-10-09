ed506ec6500284453ac0125df0d2820e001ec06f
mrm277 <107406775+mrm277@users.noreply.github.com>
2024-10-09 10:51:09 +0100

    CHANGE: Condições no yaml

---
+++ out/_next/static/chunks/pages/sum-feba7d2db5a1b912.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[814],{5892:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/sum",function(){return t(646)}])},646:function(n){"use strict";n.exports=function(n,u){return n+u}}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5892)}),_N_E=n.O()}]);
