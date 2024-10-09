dba216bf56c072b7084dae9a2e013ac9d2eb3398
mrm277 <107406775+mrm277@users.noreply.github.com>
2024-10-09 11:21:35 +0100

    CHANGE: Adicionado eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
+  {
+    rules: {
+      "no-unused-vars": "error",
+    },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
